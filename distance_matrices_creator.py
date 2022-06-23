5ae68096ecf38d9e6d8b044bfc4025adc7079c1c
Johan Cortes <camilo4182@gmail.com>
2022-06-22 22:46:42 -0500

    DistanceMatricesCreator implemented. Documentation is missing.

---
+++ distance_matrices_creator.py
@@ -0,0 +1,39 @@
+import math
+import copy
+
+class DistanceMatricesCreator:
+
+  def create_distance_matrices(self, adj_matrices):
+    dist_matrices = dict()
+    for q, adj_matrix in adj_matrices.items():
+      q_dist_matrix = self.Floyd_Warshall(adj_matrix)
+      dist_matrices[q] = q_dist_matrix
+    return dist_matrices
+
+  def set_up_initial_matrix(self, adj_matrix):
+    dist_matrix = copy.deepcopy(adj_matrix)
+    INF = math.inf
+    for i in range(len(dist_matrix)):
+      for j in range(len(dist_matrix)):
+        if i != j:
+          if adj_matrix[i][j] == 0:
+            dist_matrix[i][j] = INF
+        else:
+          dist_matrix[i][j] = 0
+        j += 1
+      i += 1
+    return dist_matrix
+
+  def Floyd_Warshall(self, adj_matrix):
+    
+    dist_matrix = self.set_up_initial_matrix(adj_matrix)
+    for k in range(len(adj_matrix)):
+      for i in range(len(adj_matrix)):
+        for j in range(len(adj_matrix)):
+          if i != j and i != k and j != k:
+            if dist_matrix[i][j] > (dist_matrix[i][k] + dist_matrix[k][j]):
+              dist_matrix[i][j] = dist_matrix[i][k] + dist_matrix[k][j]
+          j += 1
+        i += 1
+      k += 1
+    return dist_matrix
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from classifier_main_module import execute_k_fold
 from simplicial_complex_creator import SimplicialComplexCreator
 from adjacency_matrices_creator import AdjacencyMatricesCreator
+from distance_matrices_creator import DistanceMatricesCreator
 import pandas as pd
 import numpy as np
 
@@ -11,16 +12,19 @@ if __name__ == '__main__':
   splits = 4
   shuffle = False
   i = 0
-  for train, test in execute_k_fold(DATASET_PATH, splits, shuffle):
-    simplicial_complex_creator = SimplicialComplexCreator()
-    simplicial_complexes = simplicial_complex_creator.create_simplicial_complex(train)
-    adjacency_matrices_creator = AdjacencyMatricesCreator()
-    print('=======================================')
-    for k in simplicial_complexes:
-      print('Complex:', k.get_name())
-      adj_matrices_k = adjacency_matrices_creator.create_adjacency_matrices(k, k.get_incidence_matrix())
-      for key, value in adj_matrices_k.items():
-        print('q-adjacency:', key)
-        print(np.array(value))
-        print('-----------------')
-
+  train, test = next(execute_k_fold(DATASET_PATH, splits, shuffle))
+  simplicial_complex_creator = SimplicialComplexCreator()
+  simplicial_complexes = simplicial_complex_creator.create_simplicial_complex(train)
+  adjacency_matrices_creator = AdjacencyMatricesCreator()
+  distance_matrices_creator = DistanceMatricesCreator()
+  print('=======================================')
+  for k in simplicial_complexes:
+    print('Complex:', k.get_name())
+    adj_matrices_k = adjacency_matrices_creator.create_adjacency_matrices(k, k.get_incidence_matrix())
+    dist_matrices_k = distance_matrices_creator.create_distance_matrices(adj_matrices_k)
+    for q, adj_matrix in adj_matrices_k.items():
+      print('q-adjacency:', q)
+      print(np.array(adj_matrix))
+      print('q-distance:', q)
+      print(np.array(dist_matrices_k[q]))
+      print('-----------------')
