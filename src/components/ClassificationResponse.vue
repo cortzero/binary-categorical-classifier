cfe442a8baf959770f2a65d8f2b4d8a575c7ef14
Johan Camilo Cortes Ocampo <camilo4182@gmail.com>
2022-09-10 13:05:24 -0500

    Change precision to exactitud and delete the other tags

--- src/components/ClassificationResponse.vue
+++ src/components/ClassificationResponse.vue
@@ -5,18 +5,9 @@
     </div>
     <div class="my-5">
       <div class="container">
-        <div class="row">
+        <div class="row justify-content-center">
           <div class="col-3">
-            Aciertos: {{ hits }}
-          </div>
-          <div class="col-3">
-            Errores: {{ mistakes }}
-          </div>
-          <div class="col-3">
-            Total de ejemplos: {{ totalExamples }}
-          </div>
-          <div class="col-3">
-            Precisión: {{ accuracy }}%
+            Exactitud: {{ accuracy }}%
           </div>
         </div>
       </div>
@@ -39,9 +30,6 @@ export default {
   },
   data() {
     return {
-      hits: 0,
-      mistakes: 0,
-      totalExamples: 0,
       accuracy: 0.0,
       precision: 0.0,
       recall: 0.0,
@@ -53,48 +41,30 @@ export default {
     goBackHome() {
       this.$emit('nextStep', {componentName: 'UploadDataset'});
     },
-
+    calculateAccuracy() {
+      let true_cases = 0;
+      let sum = 0;
+      for (let i = 0; i < this.response.categories.length; i++) {
+        true_cases += this.response.confusion_matrix[i][i];
+        sum += this.response.confusion_matrix[i].reduce((partialSum, a) => partialSum + a, 0);
+      }
+      this.accuracy = Math.round((true_cases / sum) * 100);
+    }
   },
   created() {
-    /*this.results.forEach(result => {
-      if(result.expected_category == result.real_category) {
-        this.hits += 1;
-      }
-    });
-    this.totalExamples = this.results.length;
-    this.mistakes = this.totalExamples - this.hits;
-    this.accuracy = Math.round((this.hits / this.totalExamples) * 100);*/
-    console.log(this.response.categories);
-    console.log(this.response.confusion_matrix);
-
     this.fields = JSON.parse(JSON.stringify(this.response.categories));
 
     for (let index = 0; index < this.fields.length; index++) {
       let row = {};
-      row[" "] = this.fields[index];
+      row["Real \\ Predicted"] = this.fields[index];
       for (let j = 0; j < this.fields.length; j++) {
         row[this.fields[j]] = this.response.confusion_matrix[index][j];
       }
       this.matrix.push(row);
     }
+    this.fields.unshift("Real \\ Predicted");
 
-    this.fields.unshift(" ");
-
-    console.log(this.fields);
-    console.log(this.matrix);
-
-
-    let tp = 0;
-    let sum = 0;
-    for (let i = 0; i < this.response.categories.length; i++) {
-      for (let j = 0; j < this.response.categories.length; j++) {
-        if (i == j) {
-          tp += this.response.confusion_matrix[i][j];
-        }
-        sum += this.response.confusion_matrix[i][j];
-      }
-    }
-    this.accuracy = Math.round((tp / sum) * 100);
+    this.calculateAccuracy();
   }
 }
 </script>
